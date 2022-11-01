064ff600f9d35ea0fe1056c31d4dd6ed7eb7f28d
attowls <planting31@ajou.ac.kr>
2022-11-01 23:56:19 +0900

    Update: 헤더 추가 및 카카오 로그인 API 적용

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MoimHeader></MoimHeader>
+    <router-view/>
   </div>
+  
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
+import MoimHeader from './components/MoimHeader.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    MoimHeader,
+  },
+  methods: {
+    kakaoLogin() {
+      const params = {
+        redirectUri:'http://localhost:8080/kakaologin',
+      }
+      window.Kakao.Auth.authorize(params);
+    },
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+window.Kakao.init('562fa760eb046b2bf13d97aa8556d908');
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
