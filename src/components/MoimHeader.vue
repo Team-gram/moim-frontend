a8df4e51d6a23dacc564ffcee570133331e8df01
attowls <planting31@ajou.ac.kr>
2022-11-02 00:57:39 +0900

    responsive header

--- src/components/MoimHeader.vue
+++ src/components/MoimHeader.vue
@@ -1,8 +1,9 @@
 <template>
   <body>
+    {{ width }}, {{ height }}
     <b-navbar type="dark" variant="dark" class="shadow">
       <b-container class="navbar-container">
-        <b-row align-v="center" style="width: 100vw">
+        <b-row align-v="center" style="width: 100vw; margin-top: 20px">
           <b-col>
             <b-navbar-brand href="#" style="margin-left: 10px">
               <img src="../assets/moim_logo_fullname.png" width="120" />
@@ -16,13 +17,12 @@
               class="ml-auto login-button"
               @click="$bvModal.show('bv-modal-example')"
               style="float: right; margin-right: 10px"
-              
               >로그인
             </b-button>
           </b-col>
           <div class="w-100"></div>
           <b-col>
-            <b-navbar-nav>
+            <b-navbar-nav :fill="width <= 600">
               <b-nav-item href="#">홈</b-nav-item>
               <b-nav-item href="#">MY모임</b-nav-item>
               <b-nav-item href="#">일정관리</b-nav-item>
@@ -32,9 +32,12 @@
         </b-row>
       </b-container>
       <b-modal id="bv-modal-example" size="sm" hide-footer hide-header centered>
-        <div class="d-block text-center" style="margin-bottom: 20px; margin-top: 20px">
-      <h5>로그인 | 회원가입</h5>
-    </div>
+        <div
+          class="d-block text-center"
+          style="margin-bottom: 20px; margin-top: 20px"
+        >
+          <h5>로그인 | 회원가입</h5>
+        </div>
         <div class="d-block text-center" style="margin-bottom: 20px">
           <img
             src="../assets/kakao_login_medium_narrow.png"
@@ -48,6 +51,12 @@
 
 <script>
 export default {
+  data() {
+    return {
+      width: 0,
+      height: 0,
+    };
+  },
   methods: {
     kakaoLogin() {
       const params = {
@@ -55,6 +64,20 @@ export default {
       };
       window.Kakao.Auth.authorize(params);
     },
+    handleResize() {
+      this.width = window.innerWidth;
+      this.height = window.innerHeight;
+    },
+  },
+  created() {
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+  },
+  mounted() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
   },
 };
 </script>
@@ -64,7 +87,7 @@ export default {
   background-color: #b6d884 !important;
 }
 .login-button {
-    width: 110px !important;
+  width: 110px !important;
 }
 h5 {
   font-weight: bold;
