98e36c000ecdb928822a7bee3a874cde4cd35a4b
attowls <planting31@ajou.ac.kr>
2022-11-10 19:37:25 +0900

    검색어결과창 내 카테고리 필터 적용

--- src/components/Search/KeywordSearch.vue
+++ src/components/Search/KeywordSearch.vue
@@ -3,29 +3,31 @@
     align="center"
     style="margin-top: 20px; margin-left: 20px; margin-right: 20px"
   >
-    <h4 id="keyword-name">'입력검색어'에 대한 검색 결과</h4>
+    <h4 id="keyword-name">'{{this.oldSearchKeyword}}'에 대한 검색 결과</h4>
     <div id="searchBox">
       <b-row align-h="between">
         <b-col>
           <b-form-input
             id="searchInput"
-            v-model="searchKeyword"
+            v-model="newSearchKeyword"
             placeholder="검색어를 입력하세요."
+            v-on:keyup.enter="keywordSearch()"
           ></b-form-input>
         </b-col>
         <b-col cols="auto">
           <b-img
             id="search-button"
             :src="require('@/assets/search.png')"
+            @click="keywordSearch()"
           ></b-img>
         </b-col>
       </b-row>
     </div>
-    <div style="float: center; max-width: 1000px; height: 30px" v-if="showFilter == false">
-      <div
-        id="filter-button"
-        @click="turnOnOffFileter()"
-      >
+    <div
+      style="float: center; max-width: 1000px; height: 30px"
+      v-if="showFilter == false"
+    >
+      <div id="filter-button" @click="turnOnOffFileter()">
         <b-row>
           <b-col cols="auto" style="padding: 0px; margin: 0 2px 0 0">
             <b-img
@@ -56,6 +58,7 @@
             v-model="region1_selected"
             :options="region1_options"
             class="mb-3"
+            v-on:change="UpdateLocation(1, $event)"
             id="form-input"
           >
             <template #first>
@@ -70,6 +73,7 @@
             v-model="region2_selected"
             :options="region2_options"
             class="mb-3"
+            v-on:change="UpdateLocation(2, $event)"
             id="form-input"
           >
             <template #first>
@@ -101,6 +105,7 @@
             v-model="category1_selected"
             :options="category1_options"
             class="mb-3"
+            v-on:change="UpdateCategory($event)"
             id="form-input"
           >
             <template #first>
@@ -135,27 +140,74 @@
 </template>
 
 <script>
+import locationjson from "@/data/법정동.json";
+import categoryjson from "@/data/카테고리.json";
+
 export default {
   data() {
     return {
-      searchKeyword:"",
+      oldSearchKeyword: "검색어",
+      newSearchKeyword: "",
       region1_selected: null,
       region2_selected: null,
       region3_selected: null,
-      region1_options: ["oo시", "oo도"],
-      region2_options: ["oo군", "oo구"],
-      region3_options: ["oo읍", "oo동"],
+      region1_options: [],
+      region2_options: [],
+      region3_options: [],
       category1_selected: null,
       category2_selected: null,
-      category1_options: ["대분류1", "대분류2", "대분류3"],
-      category2_options: ["소분류1", "소분류2", "소분류3"],
+      category1_options: [],
+      category2_options: [],
       showFilter: false,
     };
   },
   methods: {
+    keywordSearch: function () {
+      if(this.newSearchKeyword.split(' ').join('') !== ""){
+        this.oldSearchKeyword = this.newSearchKeyword;
+      this.newSearchKeyword = "";
+      // 입력값에 대한 검색 결과 요청
+      }
+    },
     turnOnOffFileter: function () {
       this.showFilter = !this.showFilter;
     },
+    UpdateLocation: function (num, event) {
+      if (num == 1) {
+        this.region2_options.splice(0);
+        for (var index in locationjson[event]) {
+          console.log(index);
+          this.region2_options.push(index);
+        }
+        this.region2_options.sort();
+      } else {
+        this.region3_options.splice(0);
+        for (index in locationjson[this.region1_selected][event]) {
+          this.region3_options.push(
+            locationjson[this.region1_selected][event][index]
+          );
+        }
+        this.region3_options.sort();
+      }
+    },
+    UpdateCategory: function (event) {
+      this.category2_options.splice(0);
+      // var newEvent = event.replaceAll('/','-');
+      for (var cat_index in categoryjson[event.replaceAll('/','-')]) {
+        this.category2_options.push(categoryjson[this.category1_selected.replaceAll('/','-')][cat_index].replaceAll('-','/'));
+      }
+      this.category2_options.sort();
+    },
+  },
+  created() {
+    for (var index_location in locationjson) {
+      this.region1_options.push(index_location);
+    }
+    this.region1_options.sort();
+    for (var index_category in categoryjson) {
+      this.category1_options.push(index_category.replaceAll('-','/'));
+    }
+    this.category1_options.sort();
   },
 };
 </script>
