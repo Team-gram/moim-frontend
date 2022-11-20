8c21b73386f2795101ecb4bfbd216cbd5ab4e2a8
Lee jin ki <dlwlsrl99@gmail.com>
2022-11-20 17:54:33 +0900

    logout

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,46 +1,42 @@
 <template>
-  <kalendar :configuration="calendar_settings" :events.sync="events" />
+		<kalendar :configuration="calendar_settings" :events="events"/>
 </template>
 <script>
 import { Kalendar } from 'kalendar-vue';
 export default {
-name:"MoimTest",
-components: {
-  Kalendar,
-  },
-  data(){
-    return{
-      events: [
+  name: "MoimTest",
+	components: {
+		Kalendar,
+	},
+	data() {
+		return {
+      events:[
         {
-          from: '2020-03-18T18:00:00Z',
-          to: '2020-03-18T19:00:00Z',
-          data: 'Event 1',
-        },
-        {
-          from: '2020-03-18T19:00:00Z',
-          to: '2020-03-18T21:00:00Z',
-          data: 'Olive & Friends',
+        from: '2020-03-18T18:00:00Z',
+        to: '2020-03-18T19:00:00Z',
+        data: 'Event 1',
         },
       ],
-      calendar_settings: {
-        style: 'material_design',
-        view_type: 'week',
-        cell_height: 20,
-        scrollToNow: true,
-        start_day: new Date().toISOString(),
-        read_only: false,
-        day_starts_at: 0,
-        day_ends_at: 24,
-        overlap: true,
-        hide_dates: ['2019-10-31'], // Spooky
-        hide_days: [6],
-        past_event_creation: true
-      },
-      new_appointment: {
-        title: null,
-        description: null
-      }
-    }
-  },
-}
+			calendar_settings: {
+				view_type: 'week',
+				cell_height: 10,
+				scrollToNow: false,
+				hourlySelection: false,
+				start_day: new Date().toISOString(),
+				military_time: false,
+				read_only: false,
+				day_starts_at: 6,
+				day_ends_at: 24,
+				overlap: true,
+				hide_dates: ['2019-08-09'],
+				hide_days: [],
+				past_event_creation: true,
+			},
+			new_appointment: {},
+      
+		};
+	},
+  created(){
+  }
+};
 </script>
