9764b8bc65b0c68995bcba15b702abbb155b9dea
attowls <planting31@ajou.ac.kr>
2022-11-27 23:38:52 +0900

    Update: 헤더 변경(폰트 추가(NanumBarunGothic), FontAwesome설치(npm i 필요)

--- src/views/moimIntro.vue
+++ src/views/moimIntro.vue
@@ -215,7 +215,6 @@ export default {
     },
 
     async checkNotJoinedMoim(id) {
-
       let myMoim = await MyMoimList(this.$cookies.get("MoimUserId"));
       if (myMoim.status === 200) {
         console.log(myMoim.data);
