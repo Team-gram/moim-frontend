54c9319bdc72aeb2ee402874ae3b67070940cda0
attowls <planting31@ajou.ac.kr>
2022-12-05 14:51:19 +0900

    Update: 메인 화면 로그인 여부에 따른 분기처리

--- src/views/userSetting.vue
+++ src/views/userSetting.vue
@@ -1,42 +1,52 @@
 <template>
   <div
     align="center"
-    style="margin-top: 10px; margin-left: 20px; margin-right: 20px"
+    style="margin-top: 80px; margin-left: 20px; margin-right: 20px"
   >
-    <div id="listBox">
-      <b-row id="listTitle" align-v="center">
-        <b-col cols="auto" style="padding: 0 0 0 15px">
-          <b-img
-          id="listIcon"
-          :src="require('@/assets/settle.png')"
-          ></b-img>
-        </b-col>
-        <b-col cols="auto">
-          <div id="listTitle" >개인 정보 설정</div>
-        </b-col>
-      </b-row>
-    </div>
-    <div id="listBox">
-      <b-row id="listTitle" align-v="center">
-        <b-col cols="auto" style="padding: 0 0 0 15px">
-          <b-img
-          id="listIcon"
-          :src="require('@/assets/poweroff.png')"
-          ></b-img>
-        </b-col>
-        <b-col cols="auto">
+    <b-row align-h="center">
+      <b-col cols="auto" style="padding: 0 5px 0 5px">
+        <div id="main-text">
+          <b id="main-text-highlight">설정</b>
+        </div>
+      </b-col>
+    </b-row>
+    <div v-if="this.$cookies.get('MoimUserId')">
+      <div id="listBox">
+        <b-row id="listTitle" align-v="center">
+          <b-col cols="auto" style="padding: 0 0 0 15px">
+            <b-img id="listIcon" :src="require('@/assets/settle.png')"></b-img>
+          </b-col>
+          <b-col cols="auto">
+            <div id="listTitle">개인 정보 설정</div>
+          </b-col>
+        </b-row>
+      </div>
+      <div id="listBox">
+        <b-row id="listTitle" align-v="center">
+          <b-col cols="auto" style="padding: 0 0 0 15px">
+            <b-img
+              id="listIcon"
+              :src="require('@/assets/poweroff.png')"
+            ></b-img>
+          </b-col>
+          <b-col cols="auto">
             <div id="listTitle">서비스 탈퇴</div>
-        </b-col>
-      </b-row>
+          </b-col>
+        </b-row>
+      </div>
     </div>
+    <b-row align-h="center" v-else>
+      <b-col>
+        <div style="margin-top: 30px; margin-bottom: 500px">
+          로그인 후 이용 가능한 서비스입니다.
+        </div></b-col
+      >
+    </b-row>
   </div>
 </template>
 <script>
-
 export default {
-  components: {
-
-  },
+  components: {},
 };
 </script>
 
